d4aa812fbbd29cf66661a1df7fd229932e38f481
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2025-04-01 22:52:48 +0900

    Update urls.py

--- steam/urls.py
+++ steam/urls.py
@@ -8,6 +8,3 @@ urlpatterns = [
     path('', include('user_profile.urls')),
     path('', include('auth.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
