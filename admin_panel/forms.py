89796f5d8041a7b19c1544fb64d5ea20cfbb6068
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2025-04-13 20:33:24 +0900

    Update forms.py

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,16 +1,68 @@
-# admin_panel/forms.py
 from django import forms
 
-from user_profile.models import TegList
-from www.models import MStatus
+from user_profile.models import *
+from .models import *
+from django.forms import inlineformset_factory
+from www.models import MGame
 
-
-class MStatusForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = MStatus
-        fields = ['name']  # Мы будем работать только с полем 'name' статуса
+        model = MComment
+        fields = ["text"]
 
-class MTeglistForm(forms.ModelForm):
+
+class TagSelectionForm(forms.ModelForm):
     class Meta:
-        model = TegList
-        fields = ['name']
+        model = Teg
+        fields = ['status']
+
+
+
+class ReplyForm(forms.ModelForm):
+    class Meta:
+        model = MReply
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'placeholder': 'Напишите ответ...',
+                'rows': 3,
+                'class': 'form-control',
+            }),
+        }
+
+
+class GameForm(forms.ModelForm):
+    category = forms.ModelMultipleChoiceField(
+        queryset=MCategory.objects.all(),
+        widget=forms.SelectMultiple(attrs={'class': 'js-example-basic-multiple'}),
+        required=True,
+        label='Categoty'
+    )
+
+    class Meta:
+        model = MGame
+        fields = ['name', 'category', 'description', 'image', 'video', 'about', 'status']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+            'about': forms.Textarea(attrs={'rows': 5}),
+            'status': forms.Select(attrs={'class': 'form-control'})  # Просто Select без Select2
+        }
+
+
+ImageFormSet = inlineformset_factory(
+    parent_model=MGame,
+    model=MImage,
+    fields=('image',),
+    extra=4,
+    can_delete=False
+)
+
+# class MultipleImageUploadForm(forms.Form):
+#     game = forms.ModelChoiceField(queryset=MGame.objects.all(), required=True)
+#     images = forms.FileField(widget=forms.FileInput(attrs={"multiple": True}), required=True)
+#
+#     def clean_images(self):
+#         images = self.cleaned_data.get('images')
+#         if isinstance(images, list):  # Убедитесь, что это список изображений
+#             return images
+#         raise forms.ValidationError("Пожалуйста, выберите несколько изображений.")
