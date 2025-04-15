a1b8e6c94ca9c5dba9ec69ba3df6a3fc0902d0ed
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2025-04-15 16:38:40 +0900

    Update settings.py

--- steam/settings.py
+++ steam/settings.py
@@ -107,9 +107,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('dorkhvusb'),
-    'API_KEY': config('433537948279212'),
-    'API_SECRET': config('eRMlDGiZha2mZTcUiMTC8-39qjQ'),
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 
 # ❌ Эти строки больше не нужны, можешь удалить или закомментировать:
