45e896e6b69b88ca4f7df1d251d434b069c4c6dd
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2025-04-15 17:01:32 +0900

    Update urls.py

--- steam/urls.py
+++ steam/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin-panel/', include('admin_panel.urls')),  # Путь для административной панели
-    path('profile/', include('user_profile.urls')),     # Путь для профиля пользователя
-    path('auth/', include('auth.urls')),                 # Путь для аутентификации
-    path('', include('www.urls')),                       # Главная страница (можно оставить пустым, но не дублировать)
+
+    path('admin-panel/', include('admin_panel.urls')),  # Путь к админ панели вашего приложения
+    path('profile/', include('user_profile.urls')),  # Путь к профилю пользователя
+    path('auth/', include('auth.urls')),  # Путь к аутентификации
+    path('', include('www.urls')),  # Главная страница (если у вас есть приложение www)
 ]
