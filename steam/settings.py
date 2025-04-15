f3b8f0a2c49a5a468b3b3679c077d71517f9c94e
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2025-04-15 18:13:04 +0900

    Update settings.py

--- steam/settings.py
+++ steam/settings.py
@@ -7,6 +7,10 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
