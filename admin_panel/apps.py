479b71b5a5189a2c81ec5ea69b03a33e54273707
aklaaaaa <144097906+ayklaaa@users.noreply.github.com>
2025-05-12 23:41:50 +0900

    Update apps.py

--- admin_panel/apps.py
+++ admin_panel/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
+from django.template import engines
+from django.template.library import Library
 
+def patch_jazzmin_length_is():
+    django_engine = engines['django']
+    lib = django_engine.engine.template_libraries.get('django.templatetags.future', Library())
+    
+    @lib.filter
+    def length_is(value, arg):
+        return len(value) == int(arg)
+    
+    django_engine.engine.template_libraries['django.templatetags.future'] = lib
+
+class YourAppConfig(AppConfig):
+    def ready(self):
+        patch_jazzmin_length_is()
 
 class AdminPanelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
